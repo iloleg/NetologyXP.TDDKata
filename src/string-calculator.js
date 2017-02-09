df1734c1958deb2e78f736e409ac9e025b23aaa2
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-02-10 01:26:51 +0300

    3 - return single number if input is that number. Refactor

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,12 +1,24 @@
 "use strict";
 
 class Calculator {
+    constructor() {
+        this._defaultValue = 0;
+    }
+
     add(numbers) {
-        if (!numbers) {
-            return 0;
+        if (this._isEmpty(numbers)) {
+            return this._defaultValue;
         }
 
+        return this._parseSingleNumber(numbers);
+    }
+
+    _parseSingleNumber(numbers) {
         return parseInt(numbers, 10);
+    }
+
+    _isEmpty(numbers) {
+        return !numbers;
     }
 }
 
