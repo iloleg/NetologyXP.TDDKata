d50286221ebdbd28e4facf6c945b2cd0c05a0aff
Oleg <il.oleg@outlook.com>
2017-02-20 14:53:06 +0200

    one number

--- Homework/TDDKadaTest.js
+++ Homework/TDDKadaTest.js
@@ -1,11 +1,32 @@
 "use strict";
 var assert = require('chai').assert;
 
-
-function add(){
-    return 0
+function add(value) {
+    if (value == "") {
+        return 0
+    }
+    return value
 }
 
 suite('String Calculator testing', function () {
+    suite('without init number', function () {
+        test('Empty value will return 0', function () {
+            let input = '';
+
+            let sum = add(input);
+
+            assert.equal(0, sum);
+        });
+    })
+
+    suite('one init number', function () {
+        test('1 value will return 1', function () {
+            let input = 1;
+
+            let sum = add(input);
+
+            assert.equal(1, sum);
+        });
+    })
 
 });
